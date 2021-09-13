8acec83c6191410c1f8244e3edbd24b9c0d8f02f
ronak-supraoracles <ronak.s@novuscode.com>
2021-09-13 10:04:32 +0530

    secret_to_vec

--- node/src/supra_subcommands.rs
+++ node/src/supra_subcommands.rs
@@ -1,25 +1,28 @@
 use structopt::StructOpt;
 use bs58::{decode, Alphabet};
+use hex;
+use libp2p_core::{identity::{ed25519,PublicKey}};
 
 #[derive(Debug, StructOpt)]
-pub struct PeerIdHexCmd {
+pub struct SecretKeyHexCmd {
     /// Provides Hex version of PeerId as base58.
     #[structopt()]
-    pub peer_id: String,
+    pub secret_key: String,
 }
 
-impl PeerIdHexCmd {
-    pub fn convert_to_hex(&self) -> Result<(), sc_cli::Error> {
-        println!("Converting Peer ID in base58 to hex");
+impl SecretKeyHexCmd {
+    pub fn convert_to_peer_vec(&self) -> Result<(), sc_cli::Error> {
+        let bytes = hex::decode(&self.secret_key).unwrap();
+        let secret = ed25519::SecretKey::from_bytes(bytes).unwrap();
+        let keypair = ed25519::Keypair::from(secret);
+        let node_secret = hex::encode(keypair.secret().as_ref());
+        let peer_id = PublicKey::Ed25519(keypair.public()).into_peer_id();
+        let vec_of_peer = bs58::decode(peer_id.to_base58()).into_vec().unwrap();
 
-        let decoded_peer_id = decode(&self.peer_id)
-            .with_alphabet(Alphabet::BITCOIN)
-            .into_vec()
-            .unwrap();
-
-        println!("Bytes: {:?}", decoded_peer_id);
-
-        println!("Hex: {:?}", hex::encode(decoded_peer_id));
+        println!("PeerId: {:?}", peer_id);
+        println!("Node Key: {:?}", node_secret);
+        println!("PeerId Vector:{:?}",&vec_of_peer);
+        println!("peerId Hex:{:?}", hex::encode(&vec_of_peer));
 
         Ok(())
     }
