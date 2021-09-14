b72ab6b2e3d209c2976d252fcdcc426ee32e10f7
Pratik Tripathy <mail@pratik.live>
2021-09-15 02:41:33 +0530

    - WIP - Reorg

--- scripts/create-authority-nodes.sh
+++ scripts/create-authority-nodes.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+# shellcheck disable=SC2005
+
 # Output files
 NODE1_KEY_FILE="node1.key"
 NODE2_KEY_FILE="node2.key"
@@ -11,7 +13,7 @@ NODE2_WS_PORT=9946
 
 main()  {
   # TODO:
-  # Check if `local` works inside our docker containers
+  # Check if `local` works inside docker containers
   local supra=$1
   local pass_phrase
   local node1_key
@@ -52,11 +54,11 @@ main()  {
   sed -i '/palletBalances/,+54d' "${CHAIN_SPEC_FILE}"
 
   # Node1 - Add peerid vector & Public Key to chainSpec
-  peer_id_vec=$(generate_decoded_peerid_file "${NODE1_KEY_FILE}")
+  peer_id_vec=$(generate_decoded_peer_id_file "${NODE1_KEY_FILE}")
   search_skip_delete_insert "supraAuthorization" 3 39 "${peer_id_vec}," "${CHAIN_SPEC_FILE}"
 
   # Node2 - Add peerid vector & Public Key to chainSpec
-  peer_id_vec=$(generate_decoded_peerid_file "${NODE2_KEY_FILE}")
+  peer_id_vec=$(generate_decoded_peer_id_file "${NODE2_KEY_FILE}")
   search_skip_delete_insert "supraAuthorization" 7 39 "${peer_id_vec}," "${CHAIN_SPEC_FILE}"
 
   # Generate the rawChainSpec.json
@@ -64,10 +66,12 @@ main()  {
 
   # TODO
   ${supra} --base-path /tmp/one --chain "${RAW_CHAIN_SPEC_FILE}" --port 30333 --ws-port "$NODE1_WS_PORT" --rpc-port 9933 $NODE_COMMON_PARAMS --node-key "$(get_node_key ${NODE1_KEY_FILE})" --one &
-  sleep 10
   ${supra} --base-path /tmp/two --chain "${RAW_CHAIN_SPEC_FILE}" --port 30334 --ws-port "$NODE2_WS_PORT" --rpc-port 9934 $NODE_COMMON_PARAMS --node-key "$(get_node_key ${NODE2_KEY_FILE})" --two --bootnodes /ip4/127.0.0.1/tcp/30333/p2p/"$(get_peer_id ${NODE1_KEY_FILE})" &
-  #   - Add node1's keys to the node1's websocket endpoint using CURL
-  #   - Add node2's keys to the node2's websocket endpoint using CURL
+  echo "Execution continues"
+  #   - Add node1's AURA & GRANDPA keys to the node1's websocket endpoint using CURL
+  wait
+  #   - Add node2's AURA & GRANDPA keys to the node2's websocket endpoint using CURL
+  wait
   #   - Restart both the nodes
 }
 
@@ -79,6 +83,26 @@ get_peer_id(){
 get_node_key() {
   local node_key="$(sed -n 3p node1.key | cut -f2 -d : | xargs)"
   echo "${node_key##0x}"
+}
+
+get_public_key() {
+  echo "$(tail -1 $1 | cut -f2 -d : | xargs)"
+}
+
+get_mnemonics() {
+  echo "$(head -1 $1 | cut -f2 -d : | xargs)"
+}
+
+generate_decoded_peer_id_file() {
+  local key_file=$1
+
+  # Public key (hex) from `subkey generate --scheme sr25519` = node_key
+  local node_key="$(get_node_key $key_file)"
+
+  # Decode the Node-Key to get PeerID
+  ${supra} decode-public-key ${node_key} > "${key_file%.*}"_decoded.key
+
+  echo "$(tail -1 "${key_file%.*}"_decoded.key | cut -f2 -d : | xargs)"
 }
 
 search_skip_delete_insert() {
@@ -115,26 +139,6 @@ search_skip_replace() {
 
   local replace_text_at_line_number=$((match_found_line_number + number_of_lines_to_skip))
   sed -i "${replace_text_at_line_number}s/.*/${replace_text}/" "$file"
-}
-
-get_public_key() {
-  echo "$(tail -1 $1 | cut -f2 -d : | xargs)"
-}
-
-get_mnemonics() {
-  echo "$(head -1 $1 | cut -f2 -d : | xargs)"
-}
-
-generate_decoded_peerid_file() {
-  local key_file=$1
-
-  # Public key (hex) from `subkey generate --scheme sr25519` = node_key
-  local node_key="$(get_node_key $key_file)"
-
-  # Decode the Node-Key to get PeerID
-  ${supra} decode-public-key ${node_key} > "${key_file%.*}"_decoded.key
-
-  echo "$(tail -1 "${key_file%.*}"_decoded.key | cut -f2 -d : | xargs)"
 }
 
 main "$@"
