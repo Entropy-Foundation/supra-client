046ed80d12006887c76f02178b09aa8aba6d545a
Pratik Tripathy <mail@pratik.live>
2021-09-15 02:27:01 +0530

    - WIP - Start 2 nodes in parallel

--- scripts/create-authority-nodes.sh
+++ scripts/create-authority-nodes.sh
@@ -3,21 +3,24 @@
 # Output files
 NODE1_KEY_FILE="node1.key"
 NODE2_KEY_FILE="node2.key"
+CHAIN_SPEC_FILE="chainSpec.json"
+RAW_CHAIN_SPEC_FILE="rawChainSpec.json"
+NODE_COMMON_PARAMS="--no-prometheus --no-telemetry --rpc-methods Unsafe --rpc-cors all"
+NODE1_WS_PORT=9945
+NODE2_WS_PORT=9946
 
 main()  {
   # TODO:
   # Check if `local` works inside our docker containers
-  local supra
+  local supra=$1
   local pass_phrase
   local node1_key
   local node2_key
   local gran_key
   local peer_id_vec
 
-  supra=$1
-
   # Generate the default chainSpec.json
-  ${supra} build-spec --disable-default-bootnode --chain local > chainSpec.json 2> /dev/null && echo "Generated Chain Spec"
+  ${supra} build-spec --disable-default-bootnode --chain local > "${CHAIN_SPEC_FILE}" 2> /dev/null && echo "Generated Chain Spec"
 
   # Node1 - AURA and GRANDPA keys
   subkey generate --scheme sr25519 > ${NODE1_KEY_FILE}
@@ -29,8 +32,8 @@ main()  {
   gran_key="$(get_public_key ${NODE1_KEY_FILE%.*}_grandpa.key)"
 
   # Node1 - Add Aura & Grandpa Keys to chainSpec
-  search_skip_replace "palletAura" 2 '"'"$node1_key"'",' "chainSpec.json"
-  search_skip_replace "palletGrandpa" 3 '"'"$gran_key"'",' "chainSpec.json"
+  search_skip_replace "palletAura" 2 '"'"$node1_key"'",' "${CHAIN_SPEC_FILE}"
+  search_skip_replace "palletGrandpa" 3 '"'"$gran_key"'",' "${CHAIN_SPEC_FILE}"
 
   # Node2 - AURA and GRANDPA keys
   subkey generate --scheme sr25519 > ${NODE2_KEY_FILE}
@@ -42,29 +45,40 @@ main()  {
   gran_key=$(get_public_key "${NODE2_KEY_FILE%.*}_grandpa.key")
 
   # Node2 - Add Aura & Grandpa Keys to chainSpec
-  search_skip_replace "palletAura" 3 '"'"$node2_key"'"' "chainSpec.json"
-  search_skip_replace "palletGrandpa" 7 '"'"$gran_key"'",' "chainSpec.json"
+  search_skip_replace "palletAura" 3 '"'"$node2_key"'"' "${CHAIN_SPEC_FILE}"
+  search_skip_replace "palletGrandpa" 7 '"'"$gran_key"'",' "${CHAIN_SPEC_FILE}"
 
   # Remove palletBalances & palletSudo sections
-  sed -i '/palletBalances/,+54d' chainSpec.json
+  sed -i '/palletBalances/,+54d' "${CHAIN_SPEC_FILE}"
 
   # Node1 - Add peerid vector & Public Key to chainSpec
   peer_id_vec=$(generate_decoded_peerid_file "${NODE1_KEY_FILE}")
-  search_skip_delete_insert "supraAuthorization" 3 39 "${peer_id_vec}," "chainSpec.json"
+  search_skip_delete_insert "supraAuthorization" 3 39 "${peer_id_vec}," "${CHAIN_SPEC_FILE}"
 
   # Node2 - Add peerid vector & Public Key to chainSpec
   peer_id_vec=$(generate_decoded_peerid_file "${NODE2_KEY_FILE}")
-  search_skip_delete_insert "supraAuthorization" 7 39 "${peer_id_vec}," "chainSpec.json"
+  search_skip_delete_insert "supraAuthorization" 7 39 "${peer_id_vec}," "${CHAIN_SPEC_FILE}"
 
   # Generate the rawChainSpec.json
-  ${supra} build-spec --chain=chainSpec.json --raw --disable-default-bootnode > rawChainSpec.json 2> /dev/null && echo "Generated Raw Chain Spec"
+  ${supra} build-spec --chain="${CHAIN_SPEC_FILE}" --raw --disable-default-bootnode > "${RAW_CHAIN_SPEC_FILE}" 2> /dev/null && echo "Generated Raw Chain Spec"
 
   # TODO
-  #   - Start the bootnode (node1)
-  #   - Start the 2nd node (node2) using the configurations of the bootnode
+  ${supra} --base-path /tmp/one --chain "${RAW_CHAIN_SPEC_FILE}" --port 30333 --ws-port "$NODE1_WS_PORT" --rpc-port 9933 $NODE_COMMON_PARAMS --node-key "$(get_node_key ${NODE1_KEY_FILE})" --one &
+  sleep 10
+  ${supra} --base-path /tmp/two --chain "${RAW_CHAIN_SPEC_FILE}" --port 30334 --ws-port "$NODE2_WS_PORT" --rpc-port 9934 $NODE_COMMON_PARAMS --node-key "$(get_node_key ${NODE2_KEY_FILE})" --two --bootnodes /ip4/127.0.0.1/tcp/30333/p2p/"$(get_peer_id ${NODE1_KEY_FILE})" &
   #   - Add node1's keys to the node1's websocket endpoint using CURL
   #   - Add node2's keys to the node2's websocket endpoint using CURL
   #   - Restart both the nodes
+}
+
+get_peer_id(){
+  local key_file=$1
+  echo "$(sed -n 2p ${key_file%.*}_decoded.key | cut -f2 -d : | xargs)"
+}
+
+get_node_key() {
+  local node_key="$(sed -n 3p node1.key | cut -f2 -d : | xargs)"
+  echo "${node_key##0x}"
 }
 
 search_skip_delete_insert() {
@@ -104,23 +118,23 @@ search_skip_replace() {
 }
 
 get_public_key() {
-  echo "$(tail -1 $1 | awk -F ":" '{printf("%s", $2)}' | xargs)"
+  echo "$(tail -1 $1 | cut -f2 -d : | xargs)"
 }
 
 get_mnemonics() {
-  echo "$(head -1 $1 | awk -F ':' 'NF {printf ("%s", $2)}' | xargs)"
+  echo "$(head -1 $1 | cut -f2 -d : | xargs)"
 }
 
 generate_decoded_peerid_file() {
   local key_file=$1
 
   # Public key (hex) from `subkey generate --scheme sr25519` = node_key
-  local node_key="$(sed -n 3p ${key_file} | awk -F ":" '{printf("%s", $2)}' | xargs)"
+  local node_key="$(get_node_key $key_file)"
 
   # Decode the Node-Key to get PeerID
-  ${supra} decode-public-key ${node_key##0x} > "${key_file%.*}"_decoded.key
+  ${supra} decode-public-key ${node_key} > "${key_file%.*}"_decoded.key
 
-  echo "$(tail -1 "${key_file%.*}"_decoded.key | awk -F ':' 'NF {printf ("%s", $2)}' | xargs)"
+  echo "$(tail -1 "${key_file%.*}"_decoded.key | cut -f2 -d : | xargs)"
 }
 
 main "$@"
